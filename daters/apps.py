ecd4f9bec46b05128ee39383c39cb9305b934736
daverin <dep9911@gmail.com>
2023-06-29 12:44:37 +0300

    clients model created

---
+++ daters/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DatersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "daters"
---
+++ daters/models.py
@@ -0,0 +1,28 @@
+import os
+import random
+import string
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+def photo_path(instance, filename):
+    file_extension = os.path.splitext(filename)[1]
+    chars = string.ascii_lowercase
+    randomstring = ''.join((random.choice(chars)) for x in range(12))
+    return f'avatars/{randomstring}{file_extension}'
+
+
+class DaterUser(AbstractUser):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+
+    avatar = models.ImageField(upload_to=photo_path)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
